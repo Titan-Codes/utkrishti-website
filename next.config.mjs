3505b2525a45e1404834caeb7adfe0119decfd90
Titan-Codes <vedansh1sharma2005@gmail.com>
2024-03-04 02:04:29 +0530

    configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      // Fix "fs" module not found issue
+      if (!isServer) {
+        config.resolve.fallback = {
+          fs: false,
+        };
+      }
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
